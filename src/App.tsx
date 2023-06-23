74eab66a43d52e7784b199c4c3697e148eaadfcc
dhwani22 <dhwanisheth2202@gmail.com>
2023-06-23 13:02:41 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate replace to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contact-us" element={<Contact />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
