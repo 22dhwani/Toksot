991a5d063e45db74513386535b618947b8a75f03
Durva <durvabrahmbhatt2016@gmail.com>
2023-06-26 16:50:57 +0530

    changes

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -4,7 +4,9 @@ const Topbar = () => {
   return (
     <div className="bg-white py-4 md:px-28 xs:px-5 sticky top-0 w-full">
       <div className="flex items-center justify-center">
-        <img src={topImage} alt="Logo" />
+        <a href="/home">
+          <img src={topImage} alt="Logo" />
+        </a>
       </div>
     </div>
   );
