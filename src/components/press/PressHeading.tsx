17c014dafda88f4d95817960b363abdee5bfe890
dhwani22 <dhwanisheth2202@gmail.com>
2023-06-27 15:38:07 +0530

    changes

--- src/components/press/PressHeading.tsx
+++ src/components/press/PressHeading.tsx
@@ -1,9 +1,7 @@
 function PressHeading() {
   return (
     <div className="bg-slate-100 py-6 lg:px-96 xs:px-10 w-full">
-      <div className="text-black text-4xl font-thin font-oswald ">
-        PRESS OKKKNJ
-      </div>
+      <div className="text-black text-4xl font-thin font-oswald ">PRESS</div>
     </div>
   );
 }
