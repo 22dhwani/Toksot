89ef3d4bcf494c113ead9869f8d62c098d2bfcb6
Durva <durvabrahmbhatt2016@gmail.com>
2023-06-27 15:37:15 +0530

    checl

--- src/components/press/PressHeading.tsx
+++ src/components/press/PressHeading.tsx
@@ -1,9 +1,13 @@
 function PressHeading() {
   return (
     <div className="bg-slate-100 py-6 lg:px-96 xs:px-10 w-full">
-      <div className="text-black text-4xl font-thin font-oswald ">PRESS</div>
+      <div className="text-black text-4xl font-thin font-oswald ">
+        PRESS OKKKNJ
+      </div>
     </div>
   );
 }
 
 export default PressHeading;
+
+//dnbsudbjdsvcdhszxcjesvfjhedsvfhgsd
